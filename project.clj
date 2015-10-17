b068b9eee259886381902974915d20691cea2dcc
trikitrok <trikitrok@gmail.com>
2015-10-17 21:55:12 +0200

    Example in chapter 1

---
+++ project.clj
@@ -0,0 +1,21 @@
+(defproject guestbook "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.5"]
+                 [ring-server "0.3.1"]
+                 ;;JDBC dependecies
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [org.xerial/sqlite-jdbc "3.7.2"]]
+  :plugins [[lein-ring "0.8.12"]]
+  :ring {:handler guestbook.handler/app
+         :init guestbook.handler/init
+         :destroy guestbook.handler/destroy}
+  :profiles
+  {:uberjar {:aot :all}
+   :production
+   {:ring
+    {:open-browser? false, :stacktraces? false, :auto-reload? false}}
+   :dev
+   {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.3.1"]]}})
