8ad1f215c0b4166cf6288ebaf13d68c65007762a
trikitrok <trikitrok@gmail.com>
2015-11-03 17:53:33 +0100

    Adding a handle for not allowed methods

--- Chapter3/liberator-service/src/liberator_service/routes/home.clj
+++ Chapter3/liberator-service/src/liberator_service/routes/home.clj
@@ -1,10 +1,17 @@
 (ns liberator-service.routes.home
   (:require [compojure.core :refer :all]
             [liberator.core
-             :refer [defresource]]))
+             :refer [defresource request-method-in]]))
+
+(defn- used-request-method [context]
+  (get-in context [:request :request-method]))
 
 (defresource home
-  :allowed-methods [:get]
+  :service-available? true
+  :method-allowed? (request-method-in :get)
+  :handle-method-not-allowed
+  (fn [context]
+    (str (used-request-method context) " is not allowed"))
   :handle-ok "Hello World!"
   :etag "fixed-etag"
   :available-media-types ["text/plain"])
