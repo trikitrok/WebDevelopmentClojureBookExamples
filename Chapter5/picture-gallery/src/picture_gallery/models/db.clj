487ed1127c9bc711d41e805aa762af13d9996b12
trikitrok <trikitrok@gmail.com>
2015-12-04 20:49:01 +0100

    Created users table

---
+++ Chapter5/picture-gallery/src/picture_gallery/models/db.clj
@@ -0,0 +1,10 @@
+(ns picture-gallery.models.db
+  (:require [clojure.java.jdbc :as sql]))
+
+(def db
+  {:subprotocol "postgresql"
+   :subname "//localhost/gallery"
+   :user "admin"
+   :password "admin"})
+
+
