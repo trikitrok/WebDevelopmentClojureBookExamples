0b297cda72e0fa3e5634084547e53f61a0480568
trikitrok <trikitrok@gmail.com>
2015-12-08 20:43:23 +0100

    Extracted user and guest menues

--- Chapter5/picture-gallery/src/picture_gallery/views/layout.clj
+++ Chapter5/picture-gallery/src/picture_gallery/views/layout.clj
@@ -11,13 +11,21 @@
      (include-css "/css/screen.css")]
     [:body content]))
 
+(defn guest-menu []
+  [:div (link-to "/register" "register")
+   (form-to [:post "/login"]
+            (text-field {:placeholder "screen name"} "id")
+            (password-field {:placeholder "password"} "pass")
+            (submit-button "login"))])
+
+(defn user-menu [user]
+  (list
+    [:div (link-to "/upload" "upload images")]
+    [:div (link-to "/logout" (str "logout " user))]))
+
 (defn common [& content]
   (base
     (if-let [user (session/get :user)]
-      [:div (link-to "/logout" (str "logout " user))]
-      [:div (link-to "/register" "register")
-       (form-to [:post "/login"]
-                (text-field {:placeholder "screen name"} "id")
-                (password-field {:placeholder "password"} "pass")
-                (submit-button "login"))])
+      (user-menu user)
+      (guest-menu))
     content))
